21931ba6f4163b9cd66377fd4620a2e96893ed27
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-17 16:01:23 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,8 +9,8 @@ class MP3Importer
     @files = Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
   end
   
-  def import(list_of_filenames)
-    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+  def import
+    self.files.each{ |filename| Song.new_by_filename(filename) }
   end
   
 end
